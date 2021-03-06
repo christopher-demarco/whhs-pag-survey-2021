b539f45576721c0991cda4ac00fab25dcb49d91d
Christopher DeMarco <cdemarco@gmail.com>
2021-03-06 01:18:47 -0500

    Fix a bug, add version string

--- munge.py
+++ munge.py
@@ -4,10 +4,13 @@
 # Breakdown of cast/crew/orchestra/just for fun responses
 # Shows that are not yet represented at all
 
+from datetime import date
 import sys
 
 import numpy as np
 import pandas as pd
+
+version = "2.0.0"
 
 with open('responses.csv', 'r') as infile:
     with open('responses_preprocessed.csv' ,'w') as outfile:
@@ -34,7 +37,7 @@ col_names = [
     'crew_in',
     'crew_specialty',
     'crew_experiences',
-    'pit_in',
+    'orchestra_in',
     'instruments_since',
     'instruments_since_bring',
     'instruments_since_nobring',
@@ -46,24 +49,36 @@ col_names = [
 df = pd.read_csv('responses_preprocessed.csv', quotechar='"', names=col_names, skiprows=[0])
 df = df.replace(np.nan, '')
 
-
 stdout_orig = sys.stdout
 
+def indent(input, num=1, char=' '): return num * 4 * char + input
+
+def version_str(): return(f"Revision {version} ({date.today().strftime('%d %b %Y')})")
+
 ##### Attendance
-def fmt_for_attendance(df):
-    output = 8*" " + f"{df['firstname']} {df['lastname_now']} ({df['lastname_school']})"
+def fmt_person(df):
+    output = indent(f"{df['firstname']} {df['lastname_now']} ({df['lastname_school']})", 0)
     output += "\n"
     if df['cast_in'] != '':
-        output += 12*" " + f"Cast: {df['cast_in']}"
-    if df['pit_in'] != '':
-        output += 12*" " + f"Pit: {df['pit_in']}"
+        output += indent(f"Cast: {df['cast_in']}", 3)
+    if df['orchestra_in'] != '':
+        output += indent(f"Orchestra: {df['orchestra_in']}", 3)
     if df['crew_in'] != '':
-        output += 12*" " + f"Crew: {df['crew_in']}"
+        output += indent(f"Crew: {df['crew_in']}", 3)
     return output
+
+def print_people(people, interest):
+    if int(len(people)) > 0:
+        print(indent(f"Interested in {interest} ({len(people)} total):"))
+        for i, person in people.iterrows():
+            print(indent(fmt_person(person), 2))
+
+
 
 with open('out/registrants-by-attendance.txt', 'w') as fh:
     sys.stdout = fh
     print("Registrants by attendance:")
+    print(version_str())
     print()
     for attendance in [
             "Absolutely!  I'll be there if it happens!",
@@ -72,39 +87,28 @@ with open('out/registrants-by-attendance.txt', 'w') as fh:
         people = df[df['ready'] == attendance]
         print(f"{attendance} ({len(people)} total)")
 
-        interested_people = people[people['interest'] == 'Cast']
+        interested_people = people[people['interest'] == 'Lead']
         interested_people = interested_people[interested_people['lead'] == 'Yes']
-        if int(len(interested_people)) > 0:
-            print(f"    Interested in Lead ({len(interested_people)} total):")
-            for i, person in interested_people.iterrows():
-                print(fmt_for_attendance(person))
+        print_people(interested_people, 'Cast')
 
         interested_people = people[people['interest'] == 'Cast']
         interested_people = interested_people[interested_people['lead'] == 'Would accept one if necessary']
-        if int(len(interested_people)) > 0:
-            print(f"    Interested in Lead if necessary ({len(interested_people)} total):")
-            for i, person in interested_people.iterrows():
-                print(fmt_for_attendance(person))                
+        print_people(interested_people, 'Lead if necessary')
 
         interested_people = people[people['interest'] == 'Cast']
-        if len(interested_people) > 0:
-            print(f"    Interested in Cast ({len(interested_people)} total):")
-            for i, person in interested_people.iterrows():
-                print(fmt_for_attendance(person))
+        print_people(interested_people, 'Cast')
 
-        for interest in ["Crew", "Pit", "Just showing up for the fun!"]:
+        for interest in ["Crew", "Orchestra", "Just showing up for the fun!"]:
             interested_people = people[people['interest'] == interest]
-            if len(interested_people) > 0:
-                print(f"    Interested in {interest} ({len(interested_people)} total):")
-                for i, person in interested_people.iterrows():
-                    print(fmt_for_attendance(person))
+            print_people(interested_people, interest)
+
         print()
 
     attendance = 'No, but send me the information to buy a streaming ticket!'
-    people = df[df['ready'] == attendance]
+    interested_people = df[df['ready'] == attendance]
     print(f"{attendance} ({len(people)} total)")
-    for i, person in people.iterrows():
-        print(fmt_for_attendance(person))
+    for i, person in interested_people.iterrows():
+        print(indent(fmt_person(person)))
 
 
 
@@ -112,13 +116,14 @@ with open('out/registrants-by-attendance.txt', 'w') as fh:
 with open('out/unrepresented-shows.txt', 'w') as fh:
     shows_responded = []
     sys.stdout = fh
-    for idx, row in df[['cast_in', 'crew_in', 'pit_in']].iterrows():
+    for idx, row in df[['cast_in', 'crew_in', 'orchestra_in']].iterrows():
         for subrow in row:
             for show in subrow.split(', '):
                 shows_responded.append(show)
     shows_responded = set(shows_responded)
 
     print(f"Unrepresented shows:")
+    print(version_str())
     for s in set(
             set(
                 [
@@ -156,4 +161,4 @@ with open('out/unrepresented-shows.txt', 'w') as fh:
                     "Jesus Christ Superstar 2021"
                 ]
             ) - shows_responded):
-        print(4*' '+s)
+        print(indent(s))
